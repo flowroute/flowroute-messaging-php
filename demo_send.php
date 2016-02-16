79ac7d3815d5c04d0f403ade947ec08c9c67b97d
Doug Waller <dwaller@flowroute.com>
2016-02-15 19:00:58 -0800

    modify demo_send.php to be more concise

--- demo_send.php
+++ demo_send.php
@@ -1,11 +1,6 @@
 <?php
-// Includes and required files
-require_once('vendor/apimatic/unirest-php/src/Unirest.php');
-foreach (glob('vendor/apimatic/unirest-php/src/Unirest/*.php') as $filename){require_once $filename;}
-foreach (glob('src/*.php') as $filename){require_once $filename;}
-foreach (glob('src/Controllers/*.php') as $filename){require_once $filename;}
-foreach (glob('src/Models/*.php') as $filename){require_once $filename;}
 
+require_once('vendor/autoload.php');
 use FlowrouteMessagingLib\Controllers\MessagesController;
 use FlowrouteMessagingLib\Models\Message;
 
@@ -13,8 +8,9 @@ use FlowrouteMessagingLib\Models\Message;
 // Please replace the variables with your information.
 print "Demo SMS PHP script.\n";
 
-$controller = new MessagesController('YOUR_API_KEY','YOUR_API_SECRET_KEY');
-$message = new Message('TO_PHONE_NUMBER_E164', 'FROM_PHONE_NUMBER_E164', 'Your cool new SMS message here!');
-$response = $controller->createMessage($message);
+$controller = new MessagesController('11656133','zJxOC2VNO7BIHiYTs6InrMnmluYy2dks');
+// $message = new Message('TO_PHONE_NUMBER_E164', 'FROM_PHONE_NUMBER_E164', 'Your cool new SMS message here!');
+// $response = $controller->createMessage($message);
+$response = $controller->getMessageLookup('mdr1-b334f89df8de4f8fa7ce377e06090a2e');
 
 print_r($response);
