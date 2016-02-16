9968708be93d6455f17a0f5dbc350693946fe764
Doug Waller <dwaller@flowroute.com>
2016-02-16 08:45:20 -0800

    Remove sandbox credentials

--- demo_send.php
+++ demo_send.php
@@ -8,9 +8,12 @@ use FlowrouteMessagingLib\Models\Message;
 // Please replace the variables with your information.
 print "Demo SMS PHP script.\n";
 
-$controller = new MessagesController('11656133','zJxOC2VNO7BIHiYTs6InrMnmluYy2dks');
-// $message = new Message('TO_PHONE_NUMBER_E164', 'FROM_PHONE_NUMBER_E164', 'Your cool new SMS message here!');
-// $response = $controller->createMessage($message);
-$response = $controller->getMessageLookup('mdr1-b334f89df8de4f8fa7ce377e06090a2e');
+$controller = new MessagesController('AccesKey','SecretKey');
+print_r($controller);
+ $message = new Message('TO_PHONE_NUMBER_E164', 'FROM_PHONE_NUMBER_E164', 'Your cool new SMS message here!');
+ $response = $controller->createMessage($message);
+
+//Example use of retrieving a MDR
+//$response = $controller->getMessageLookup('mdr1-b334f89df8de4f8fa7ce377e06090a2e');
 
 print_r($response);
